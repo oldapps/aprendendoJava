aac73b1b89f4118415e941afb539448b1120f92b
Andrei <Andrei@DESKTOP-UH9HBVL>
2025-01-12 21:51:32 -0300

    V1

---
+++ JavaApplication4/src/javaapplication4/Controlador.java
@@ -0,0 +1,13 @@
+package javaapplication4;
+public interface Controlador {
+   public abstract boolean ligar();
+   public abstract boolean desligar();
+   public abstract boolean abrirMenu();
+   public abstract boolean fecharMenu();
+   public abstract boolean maisVolume();
+   public abstract boolean menosVolume();
+   public abstract boolean ligarMudo();
+   public abstract boolean desligarMudo();
+   public abstract boolean play();
+   public abstract boolean pause();   
+}
