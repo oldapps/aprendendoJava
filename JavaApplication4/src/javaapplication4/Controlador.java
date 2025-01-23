5d76948890496fe5b84c09becd669d67238ae3d0
Andrei <Andrei@DESKTOP-UH9HBVL>
2025-01-22 22:27:28 -0300

    Merge origin/main into main

--- JavaApplication4/src/javaapplication4/Controlador.java
+++ JavaApplication4/src/javaapplication4/Controlador.java
@@ -1,13 +1,14 @@
 package javaapplication4;
-public interface Controlador {
-   public abstract boolean ligar();
-   public abstract boolean desligar();
-   public abstract boolean abrirMenu();
-   public abstract boolean fecharMenu();
-   public abstract boolean maisVolume();
-   public abstract boolean menosVolume();
-   public abstract boolean ligarMudo();
-   public abstract boolean desligarMudo();
-   public abstract boolean play();
-   public abstract boolean pause();   
+
+  public interface Controlador {
+    void ligar();
+    void desligar();
+    void abrirMenu();
+    void fecharMenu();
+    void maisVolume();
+    void menosVolume();
+    void ligarMudo();
+    void desligarMudo();
+    void play();
+    void pause();
 }
--- JavaApplication4/src/javaapplication4/ControleRemoto.java
+++ JavaApplication4/src/javaapplication4/ControleRemoto.java
@@ -1,113 +1,118 @@
 package javaapplication4;
-
-
-public class ControleRemoto  {
+// Classe publica "" implementa interface controlador
+public class ControleRemoto implements Controlador {
+    // variaveis declaradas
     private int volume;
     private boolean ligado;
     private boolean tocando;
 
-    
-    public void ControleRemoto() {
-        this.setVolume(50);
-        this.setLigado(false);
-        this.setTocando(false);
+    // criador de objetos com seus parametros
+    public ControleRemoto() {
+        this.volume = 50;
+        this.ligado = false;
+        this.tocando = false;
     }
-
-    private int getVolume(){
+// metodos de acesso get= buscar e set= inserir
+    private int getVolume() {
         return volume;
     }
-    private boolean getLigado(){
+
+    private boolean getLigado() {
         return ligado;
     }
-    private boolean getTocando(){
+
+    private boolean getTocando() {
         return tocando;
     }
-    
-    private void setVolume(int v){
+
+    private void setVolume(int v) {
         this.volume = v;
     }
-    private void setLigado(boolean l){
+
+    private void setLigado(boolean l) {
         this.ligado = l;
     }
-    private void setTocando(boolean t){
+
+    private void setTocando(boolean t) {
         this.tocando = t;
     }
-    
-   public void  ligar(){
-       this.ligado = true;
-       
-   }
-   public void  desligar(){
-       this.ligado = false;
-   }
-   public void  abrirMenu(){
-       if (this.getLigado() == true) {
-           System.out.println(this.getLigado());
-           System.out.println(this.getVolume());
-           System.out.println("Menu\n------------------------");
-           System.out.println("Volume " + this.getVolume() + "%");
-       } else {
-           System.out.println("Tv desligada");
-       }
-   }
-   public void  fecharMenu(){
-       System.out.println("Menu fechado");
-   }
-   public void  maisVolume(){
-       if (this.getLigado() == true) {
-           this.setVolume(this.getVolume() + 1);       
-           
-       } else {
-           System.out.println("Tv desligada");
-       }
-   }
-   public void  menosVolume(){
-       if (this.getLigado() == true) {
-           this.setVolume(this.getVolume() - 1);       
-           
-       } else {
-           System.out.println("Tv desligada");
-       }
-       
-   }
-   public void  ligarMudo(){
-       if (this.getLigado() == true && this.volume > 0) {
-           this.setVolume(0);
-           
-       } else {
-       }
-   }
-   public void  desligarMudo(){
-       if (this.getLigado() == true && this.volume == 0) {
-           this.setVolume(50);
-           
-       } else {
-       }
-   }
-   public void  play(){
-       if (this.getLigado() == true) {
-           this.setTocando(true);
-           
-       } else {
-           
-       }
-   }
-   public void  pause(){
-       if (this.getLigado() == true && this.getTocando() == true)
-           this.setTocando (false);
-       System.out.println("Pausado");
-   }
+// metodos especiais com regras
+    @Override
+    public void ligar() {
+        this.setLigado(true);
+    }
+
+    @Override
+    public void desligar() {
+        this.setLigado(false);
+    }
+
+    @Override
+    public void abrirMenu() {
+        System.out.println("-------------------MENU-------------------");
+        System.out.println("Está ligada? " + this.getLigado());
+        System.out.println("Está tocando? " + this.getTocando());
+        System.out.println("Volume: " + this.getVolume());
+        for (int i = 0; i <= this.getVolume(); i+=10){
+            System.out.print("[^]");
+        }
+        System.out.println("");
+    }
+
+    @Override
+    public void fecharMenu() {
+        System.out.println("Fechando menu...");
+    }
+
+    @Override
+    public void maisVolume() {
+        if (this.getLigado()) {
+            this.setVolume(this.getVolume() + 5);
+        } else {
+            System.out.println("TV desligada");
+        }
+    }
+
+    @Override
+    public void menosVolume() {
+        if (this.getLigado()) {
+            this.setVolume(this.getVolume() - 5);
+        } else {
+            System.out.println("TV desligada");
+        }
+    }
+
+    @Override
+    public void ligarMudo() {
+        if (this.getLigado() && this.getVolume() > 0) {
+            this.setVolume(0);
+        }
+    }
+
+    @Override
+    public void desligarMudo() {
+        if (this.getLigado() && this.getVolume() == 0) {
+            this.setVolume(50);
+        }
+    }
+
+    @Override
+    public void play() {
+        if (this.getLigado() && !(this.getTocando())) {
+            this.setTocando(true);
+            System.out.println("Reproduzindo");
+        } else {
+            System.out.println("Não foi possível reproduzir");
+        }
+    }
+
+    @Override
+    public void pause() {
+        if (this.getLigado() && this.getTocando()) {
+            this.setTocando(false);
+            System.out.println("Pausado");
+        } else {
+            System.out.println("Não foi possível pausar");
+        }
+    }
 }
-
-
-
-        
-    
-    
-    
-    
-    
-    
-    
-    
-
